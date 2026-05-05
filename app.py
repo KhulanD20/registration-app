55b8fede582810117f6c314a762729608b1a6721
KhulanD20 <Dashravdankhulan@gmail.com>
2026-05-05 12:04:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ VALUES (?, ?, ?, ?, ?)
             conn.close()
             message = "Амжилттай бүртгэгдлээ"
 
-    return render_template("register.html", message=message, error=error)
+    
+return render_template("templates/register.html", message=message, error=error)
 
 @app.route("/download")
 def download():
